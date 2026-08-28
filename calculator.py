485aa90fd9809c1f8192c3d27a1021e90fab00ef
henry-benjamin <akyambahenrybenjamin@gmail.com>
2026-08-28 10:59:32 +0300

    this is the calculator to ask price and quantity of items

---
+++ calculator.py
@@ -0,0 +1,4 @@
+price = float(input("how much is this item: "))
+quantity = int(input("how manu are there: "))
+total = price * quantity
+print(f"{quantity} items at {total:.2f} each is : {price:.2f}")
